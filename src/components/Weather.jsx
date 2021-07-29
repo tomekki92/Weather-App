315fb955b41ebd5bd7849693fac10667beb5e667
Tomas <tomas.tomas44@gmail.com>
2021-07-29 22:29:43 +0300

    get data from Meteo api, implement select section with changes according to selected option

---
+++ src/components/Weather.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+
+class Weather extends Component {
+  state = {
+    forecast: [],
+    temperature: [],
+    value: "shit",
+  };
+
+  componentDidMount = async () => {
+    const api_call = await fetch(
+      "https://cors-anywhere.herokuapp.com/https://api.meteo.lt/v1/places/vilnius/forecasts/long-term"
+    );
+    const data = await api_call.json();
+    this.setState({ forecast: data.forecastTimestamps });
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    this.setState({ value: input.value });
+    console.log(input.value);
+    const forecast = [...this.state.forecast];
+
+    forecast.map((f) => {
+      if (f.forecastTimeUtc === input.value) {
+        this.setState({ temperature: f.airTemperature + `C` });
+      }
+      return (
+        <span className="badge bg-secondary">{this.state.temperature}</span>
+      );
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Sveikutiz</h1>
+        <button className="btn btn-primary btn-">
+          Temperatūra: {this.state.temperature}
+        </button>
+        <div className="form-group">
+          <label htmlFor="forecastTimeUtc">Select the time</label>
+          <select
+            name="forecastTimeUtc"
+            id="forecastTimeUtc"
+            value={this.state.value}
+            className="form-control"
+            onChange={this.handleChange}
+          >
+            <option>{this.state.time}</option>
+            {this.state.forecast.map((option) => (
+              <option
+                key={option.forecastTimeUtc}
+                value={option.forecastTimeUtc}
+              >
+                {option.forecastTimeUtc}
+              </option>
+            ))}
+          </select>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Weather;
